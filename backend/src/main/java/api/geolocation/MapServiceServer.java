4f09c73d3678e36aeea52b87e02e00bfe170740b
Florin Zamfir <f.zamfir@student.tugraz.at>
2024-09-06 22:04:32 +0200

    added logic for fetching missing nodes

--- backend/src/main/java/api/geolocation/MapServiceServer.java
+++ backend/src/main/java/api/geolocation/MapServiceServer.java
@@ -1,8 +1,15 @@
 package api.geolocation;
 
+import api.geolocation.datamodels.Node;
+import api.geolocation.datamodels.Way;
+import api.geolocation.osm.OSMFinder;
 import api.geolocation.osm.OSMParser;
+import com.fasterxml.jackson.databind.JsonNode;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class MapServiceServer {
@@ -17,7 +24,11 @@ public class MapServiceServer {
 
         parseOSMFile();
 
-        fixInvalidEntries();
+        System.out.println("Missing nodes: " + DataStore.getInstance().getMissingNodes().size());
+        System.out.println("Missing ways:  " + DataStore.getInstance().getMissingWays().size());
+        System.out.println("Invalid ways:  " + DataStore.getInstance().getMissingWays().size());
+
+        // fixInvalidEntries();
 
         startServer();
     }
@@ -47,9 +58,72 @@ public class MapServiceServer {
     private static void fixInvalidEntries() {
         DataStore dataStore = DataStore.getInstance();
 
-        if (!dataStore.getInvalidWays().isEmpty()) {
-            System.out.println("Ways containing invalid ");
+        List<Long> toRemove = new ArrayList<>();
+
+        System.out.println("Number of invalid ways: " + dataStore.getInvalidWays().size());
+        for (var invalidWayEntry : dataStore.getInvalidWays().entrySet()) {
+
+            Way way = invalidWayEntry.getValue();
+            int nodesAdded = 0;
+            int missingNodesCount = way.getMissingNodes().size();
+            for (var missingNodeId : way.getMissingNodes()) {
+                if (dataStore.getNodes().containsKey(missingNodeId)) {
+                    System.out.println("Node fetched already! Skipping request...");
+                    continue;
+                }
+
+                try {
+                    JsonNode response = OSMFinder.fetchNode(missingNodeId);
+                    final Node missingNode = new Node();
+
+                    if (response.has("elements") && response.get("elements").isArray()) {
+                        for (JsonNode element : response.get("elements")) {
+                            if ("node".equals(element.get("type").asText())) {
+                                missingNode.setId(element.get("id").asLong());
+                                missingNode.setLat(element.get("lat").asDouble());
+                                missingNode.setLon(element.get("lon").asDouble());
+
+                                // Check if the node has tags
+                                if (element.has("tags")) {
+                                    JsonNode tags = element.get("tags");
+                                    tags.fields().forEachRemaining(entry -> missingNode.getTags().put(entry.getKey(), entry.getValue().asText()));
+                                }
+
+                                way.getNodes().add(missingNode);
+
+                                dataStore.getNodes().put(missingNodeId, missingNode);
+                                System.out.println("Node added! id = " + missingNodeId);
+
+                            }
+                        }
+                    }
+
+                } catch (Exception ex) {
+                    ex.printStackTrace(System.out);
+                }
+                nodesAdded++;
+            }
+
+            if (nodesAdded == missingNodesCount) {
+                way.getMissingNodes().clear();
+                way.setMissingNodes(null);
+                System.out.println("Missing nodes of invalid way fixed! id = " + way.getId());
+
+                toRemove.add(way.getId());
+            }
+            else
+            {
+                System.out.println("Could not fix invalid way!          id = " + way.getId());
+            }
+
         }
+
+        for (int i = 0; i < toRemove.size(); i++) {
+            dataStore.getInvalidWays().remove(toRemove.get(i));
+        }
+
+        System.out.println("Invalid ways remaining: " + toRemove.size());
+        System.out.println("Finished fixing missing entries!");
     }
 
     private static void startServer() {
