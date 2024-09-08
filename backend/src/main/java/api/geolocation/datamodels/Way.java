54b5461f82195ea12513864b2bf5ffed33ebd755
Florin Zamfir <f.zamfir@student.tugraz.at>
2024-09-08 22:40:29 +0200

    replaced constructors with factory functions and removed redundant cases

--- backend/src/main/java/api/geolocation/datamodels/Way.java
+++ backend/src/main/java/api/geolocation/datamodels/Way.java
@@ -5,7 +5,6 @@ import java.util.*;
 import api.geolocation.DataStore;
 import lombok.Data;
 import org.locationtech.jts.geom.*;
-import org.locationtech.jts.geom.impl.CoordinateArraySequenceFactory;
 
 @Data
 public class Way implements IOSMDataModel {
@@ -24,28 +23,18 @@ public class Way implements IOSMDataModel {
     public Geometry toGeometry() throws RuntimeException {
         int nodeCount = nodes.size();
 
-        if (nodeCount == 0)
-            throw new RuntimeException("Way with id " + id + " has no referenced nodes!");
+        if (nodeCount < 2)
+            throw new RuntimeException("Way with id " + id + " has less than 2 referenced nodes!");
 
         Coordinate[] coordinates = new Coordinate[nodeCount];
         for (int i = 0;  i < nodeCount; i++) {
-            if (nodes.get(i) == null)
-                throw new RuntimeException("Node referenced by way is null!!?");
-
             coordinates[i] = new Coordinate(nodes.get(i).getLon(), nodes.get(i).getLat());
         }
 
-        // TODO: check if equality check is correct
-        if (coordinates.length > 2 && Objects.equals(nodes.get(0), nodes.get((nodes.size() - 1)))){
-            LinearRing linearRing = DataStore.geometryFactory.createLinearRing(coordinates);
-            return new Polygon(linearRing, null, DataStore.geometryFactory);
-        }
+        if (coordinates.length > 2 && Objects.equals(nodes.get(0), nodes.get((nodes.size() - 1))))
+            return DataStore.geometryFactory.createLinearRing(coordinates);
 
-        if (coordinates.length == 1) {
-            return new Point(CoordinateArraySequenceFactory.instance().create(coordinates), DataStore.geometryFactory);
-        }
-
-        return new LineString(CoordinateArraySequenceFactory.instance().create(coordinates), DataStore.geometryFactory);
+        return DataStore.geometryFactory.createLineString(coordinates);
     }
 
     public List<Long> getNodeIds() {
