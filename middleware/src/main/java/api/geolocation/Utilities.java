a94f79911afe575599a3efcbef3b55e73bd228ee
Florin Zamfir <f.zamfir@student.tugraz.at>
2025-07-13 14:59:30 +0200

    expanded utilities

--- middleware/src/main/java/api/geolocation/Utilities.java
+++ middleware/src/main/java/api/geolocation/Utilities.java
@@ -5,11 +5,18 @@ import org.json.simple.parser.JSONParser;
 public class Utilities {
     public static JSONParser jsonParser = new JSONParser();
 
-    public static boolean latitudeIsValid(double latitude) {
+    public static boolean isLatitudeValid(double latitude) {
         return (-90.0 <= latitude && latitude <= 90.0);
     }
 
-    public static boolean longitudeIsValid(double longitude) {
+    public static boolean isLongitudeValid(double longitude) {
         return (-180.0 <= longitude && longitude <= 180.0);
+    }
+
+    public static boolean areBoundingBoxParametersValid(double bboxTlX, double bboxTlY, double bboxBrX, double bboxBrY) {
+        return (Utilities.isLongitudeValid(bboxTlX) &&
+                Utilities.isLatitudeValid(bboxTlY)  &&
+                Utilities.isLongitudeValid(bboxBrX) &&
+                Utilities.isLatitudeValid(bboxBrY));
     }
 }
